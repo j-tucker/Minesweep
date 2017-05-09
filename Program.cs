57cadbd9a9b8956a569ed5223ff222e296cd0b07
James <githubjt@vd0.net>
2017-05-08 20:20:25 -0700

    Clean up

--- Program.cs
+++ Program.cs
@@ -1,119 +1,93 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace minesweeper
 {
-
-    class Program
+    internal class Program
     {
+        private static readonly Random Random = new Random();
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             //var a = CreateMineLocations(10, 400);
-            var a = CreateMineField(30, 30, 100);
+            var a = CreateMineField(30, 30, 10);
             Console.WriteLine("");
             WriteGrid(a);
-
         }
 
-        static void WriteGrid(int[,] a)
+        private static void WriteGrid(int[,] a)
         {
+            var maxRows = a.GetUpperBound(0);
+            var maxColumns = a.GetUpperBound(1);
 
-            int bound0 = a.GetUpperBound(0);
-            int bound1 = a.GetUpperBound(1);
-
-            for (int variable1 = 0; variable1 <= bound0; variable1++)
+            for (var row = 0; row <= maxRows; row++)
             {
-                for (int variable2 = 0; variable2 <= bound1; variable2++)
+                for (var column = 0; column <= maxColumns; column++)
                 {
-                    var value = a[variable1, variable2];
-                    // Console.Write($"{value.ToString().PadLeft(2, ' ')}  ");
+                    var value = a[row, column];
 
                     Console.Write($"{(value == -1 ? "*" : value.ToString()).PadLeft(2, ' ')}  ");
-
                 }
                 Console.WriteLine();
             }
         }
 
-        static int[,] CreateMineField(int rows, int columns, int numberOfMines)
+        private static int[,] CreateMineField(int rows, int columns, int numberOfMines)
         {
-            int totalCells = rows * columns;
-            int totalMines = numberOfMines; // Convert.ToInt32(Math.Ceiling(totalCells * (difficulty * .01)));
-
-            //Assign mines to cells at random
+            var totalMines = numberOfMines;
+            var mineLocations = CreateMineLocations(totalMines, rows * columns).OrderBy(o => o).ToList();
 
             var mineField = new int[rows, columns];
 
-
-
-            var mineLocations = CreateMineLocations(totalMines, totalCells);
-            int currentCell = 0;
-
             //Populate mines into cells
-            for (int r = 0; r < rows; r++)
+
+            mineLocations.ForEach(mine => { mineField[mine / rows, mine % columns] = -1; });
+
+            var adjacentCells = new[,]
             {
-                for (int c = 0; c < columns; c++)
-                {
-                    currentCell++;
-                    if (mineLocations.Contains(currentCell))
-                        mineField[r, c] = -1; // -1 = You hit a mine!                    
-                    else
-                        mineField[r, c] = 0; // 0 = No Mine;
-                }
-            }
-
-
-            var adjacentCells = new int[,] {
-                                                {0,1},{0,-1}, // Side to Side
-                                                {1,0},{-1,0}, // Top and bottom
-                                                {-1,-1},{-1,1}, // top corners
-                                                {1,-1},{1,1},   // Bottom Corners
-                                            };
+                {0, 1}, {0, -1}, // Side to Side
+                {1, 0}, {-1, 0}, // Top and bottom
+                {-1, -1}, {-1, 1}, // top corners
+                {1, -1}, {1, 1} // Bottom Corners
+            };
 
 
             //Populate total mine count based on adjacent cells
-            for (int r = 0; r < rows; r++)
+            for (var r = 0; r < rows; r++)
+            for (var c = 0; c < columns; c++)
             {
-                for (int c = 0; c < columns; c++)
+                if (mineField[r, c] == -1) // skip mines
+                    continue;
+
+                var numMines = 0;
+
+                for (var i = 0; i <= adjacentCells.GetUpperBound(0); i++)
                 {
-                    int numMines = 0;
-                    if (mineField[r, c] == -1) // skip mines
+                    var rOffset = adjacentCells[i, 1];
+                    var cOffset = adjacentCells[i, 0];
+
+                    //Don't count off the board
+                    if (r + rOffset > rows - 1 || r + rOffset < 0)
                         continue;
 
-                    for (int i = 0; i <= adjacentCells.GetUpperBound(0); i++)
-                    {
-                        int cOffset = adjacentCells[i, 0];
-                        int rOffset = adjacentCells[i, 1];
+                    if (c + cOffset > columns - 1 || c + cOffset < 0)
+                        continue;
 
-                        if (r + rOffset > rows - 1 || r + rOffset < 0)
-                            continue;
+                    //Don't count non-mine spaces
+                    if (mineField[r + rOffset, c + cOffset] != -1)
+                        continue;
 
-                        if (c + cOffset > columns - 1 || c + cOffset < 0)
-                            continue;
-
-                        if (mineField[r + rOffset, c + cOffset] == -1)
-                        {
-                            numMines++;
-                            mineField[r, c] = numMines;
-                        }
-
-                    }
+                    numMines++;
                 }
+                mineField[r, c] = numMines;
             }
 
             return mineField;
         }
 
-
-        static List<int> CreateMineLocations(int totalMines, int totalCells)
+        private static List<int> CreateMineLocations(int totalMines, int totalCells)
         {
-
             var locations = new List<int>();
 
 
@@ -122,8 +96,7 @@ namespace minesweeper
 
             while (locations.Count() < totalMines)
             {
-                var random = new Random();
-                var location = random.Next(1, totalCells);
+                var location = Random.Next(0, totalCells - 1);
 
                 if (!locations.Contains(location))
                     locations.Add(location);
